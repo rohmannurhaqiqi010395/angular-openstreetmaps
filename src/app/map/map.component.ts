b225e75857d08793e7db08620df3994965b79911
rohmannurhaqiqi010395 <rohmannurhaqiqi@gmail.com>
2019-06-27 18:26:12 +0700

    long lat

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import Map from "ol/Map";
 import Tile from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import View from "ol/View";
+import Feature from "ol/Feature";
+import Point from 'ol/geom/Point';
+import { fromLonLat } from 'ol/proj.js';
 
 @Component({
   selector: 'app-map',
@@ -10,18 +15,30 @@ import Tile from "ol/layer/Tile";
 export class MapComponent implements OnInit {
 
   map;
+  indonesia;
+
 
   constructor() { }
 
   ngOnInit() {
+    this.initizationMap();
   }
 
   initizationMap(){
+    this.indonesia = new Feature ({
+      geometry: new Point(fromLonLat([-6.200000, 106.816666]))
+    });
     this.map = new Map({
       target: 'map',
       layers: [
-        new Tile
-      ]
+        new Tile ({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: fromLonLat([17.16, 26.84]),
+        zoom: 5
+      })
     })
   }
 
